d6ff6481b01ba4c07fffdbdff93e8593c220034b
Remzi Ogul TUM <amadeuwoles@gmail.com>
2022-12-07 21:45:25 +0300

    add auth navigation

--- App.js
+++ App.js
@@ -1,24 +1,10 @@
-import * as React from "react";
-import { View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
-
-const Stack = createNativeStackNavigator();
+import AuthNavigation from "./src/navigation/AuthNavigation";
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
+      <AuthNavigation />
     </NavigationContainer>
   );
 }
